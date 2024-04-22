2999f2055dda0d914eae6ea28ad6bfe2e2b5e612
Vladimir Pozharski <Baikonur96@yandex.ru>
2024-04-23 00:02:15 +0300

    ver.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,39 @@
 package org.example;
 
-import com.sun.javadoc.Doc;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 public class Main {
     public static void main(String[] args) {
-       String nppFile = Service.parsefile("");
+       String nppFile = Service.parsefile("C:\\Users\\Baiko\\NppObjAltstation\\data\\10DMB10EJ101.svg");
        Document doc = Jsoup.parse(nppFile);
-       Elements el = doc.select("image.obj_Station_stat");
+       Elements el = doc.select("image[xlink:href=\"obj_Station_stat.svg\"]");
        el.forEach(element -> {
            System.out.println("----------------");
-           System.out.println(element.text());
+           String elOnClick = element.select("rt\\:event[type=\"OnClick\"]").text();
+           int firstChar = elOnClick.indexOf("\"");
+           int  secondChar = elOnClick.indexOf("\"", firstChar + 1);
+           String kksClickNpp = elOnClick.substring(firstChar + 1, secondChar);
+           System.out.println(kksClickNpp);
+           //System.out.println(element.select("rt\\:event[type=\"OnClick\"]"));
+           System.out.println(elOnClick);
+
+
+           System.out.println("++++++++++++++++++++++++++++");
+           String elStationName = element.select("rt\\:dyn[type=\"Station_Name\"]").attr("value");
+           if (elStationName.isEmpty()){
+               System.out.println("пустой");
+               element.append("<rt:dyn type=\"Station_Name\" mode=\"constant\" value=\"&quot;0000#" + kksClickNpp + "&quot;\"/>");
+           }
+           else {
+               element.select("rt\\:dyn[type=\"Station_Name\"]").attr("value").replace(elStationName, "\"0000#" + kksClickNpp + "\"");
+           }
+
+         //  System.out.println(element.select("rt\\:dyn[type=\"Station_Name\"]").attr("value"));
+          //System.out.println(element);
            System.out.println("----------------");
        });
+       Service.WriteSVG("10DMB10EJ101NW", doc);
     }
 }
--- src/main/java/org/example/Service.java
+++ src/main/java/org/example/Service.java
@@ -1,9 +1,18 @@
 package org.example;
 
+import org.apache.commons.io.FileUtils;
+import org.jsoup.nodes.Document;
+
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 public class Service {
@@ -11,19 +20,20 @@ public class Service {
     public static String parsefile(String path) {
         StringBuilder builder = new StringBuilder();
         try {
-            List<String> lines = Files.readAllLines(Paths.get(path));
+            List<String> lines = Files.readAllLines(Paths.get(path),Charset.forName("windows-1251"));
             lines.forEach(line -> builder.append(line + "\n"));
         }catch (Exception e){
             e.printStackTrace();
         }
+        System.out.println(builder.length());
         return builder.toString();
     }
 
-    public static void WriteFile(StringBuffer buff) {
-        String path = "C:/Users/Adminsvu/Documents/FinalProject_1/SearchEngine/src/main/java/searchengine/outFiles/res.txt";
+    public static void WriteFile(String name, String body) {
+        String path = "C:\\Users\\Baiko\\NppObjAltstation\\data\\reb\\" + name + ".svg";
         try {
             PrintWriter writer = new PrintWriter(path);
-            writer.write(buff.toString());
+            writer.write(body);
             writer.flush();
             writer.close();
 
@@ -31,4 +41,27 @@ public class Service {
             e.printStackTrace();
         }
     }
+
+    public static void WriteSVG(String name, Document doc) {
+        String path = "C:\\Users\\Baiko\\NppObjAltstation\\data\\reb\\" + name + ".svg";
+        try {
+            File f = new File(path);
+            FileUtils.writeStringToFile(f, doc.html(), Charset.forName("windows-1251") );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+//    public static  void  saveSvg (String code){
+//        String documentPath = "save-to-SVG.svg";
+//
+//
+//
+//// Initialize an SVG instance from the content string
+//        com.aspose.html.dom.svg.SVGDocument document = new com.aspose.html.dom.svg.SVGDocument(code, ".");
+//
+//// Save the SVG file to a disk
+//        document.save(documentPath);
+//    }
 }
