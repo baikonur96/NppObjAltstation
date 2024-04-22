7fa6623e3ff3d373b4050dd6103651c6e9d9a8ec
Adminsvu <Baikonur96@yandex.ru>
2024-04-22 16:34:53 +0300

    ver.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import com.sun.javadoc.Doc;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       String nppFile = Service.parsefile("");
+       Document doc = Jsoup.parse(nppFile);
+       Elements el = doc.select("image.obj_Station_stat");
+       el.forEach(element -> {
+           System.out.println("----------------");
+           System.out.println(element.text());
+           System.out.println("----------------");
+       });
     }
 }
--- src/main/java/org/example/Service.java
+++ src/main/java/org/example/Service.java
@@ -1,8 +1,23 @@
 package org.example;
 
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Service {
+
+    public static String parsefile(String path) {
+        StringBuilder builder = new StringBuilder();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(path));
+            lines.forEach(line -> builder.append(line + "\n"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
 
     public static void WriteFile(StringBuffer buff) {
         String path = "C:/Users/Adminsvu/Documents/FinalProject_1/SearchEngine/src/main/java/searchengine/outFiles/res.txt";
